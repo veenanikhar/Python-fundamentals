d27e78ea24e5f89c777fb49eb584df64d477222c
veenanikhar <veenanikhar1999@gmail.com>
2024-09-17 10:58:54 +0530

    Updated local project and merged README from GitHub

--- Day1/Strings.py
+++ Day1/Strings.py
@@ -6,6 +6,38 @@ print(name[1])
 txt = "The best things in life are free!"
 print("free" in txt)
 
+# Check if Not
+print("expensive" not in txt)
+
 # String length
 print(len(name))
 
+b = "Hello, World!"
+print(b[2:5]) # 5 not included
+
+#Slice From the Start
+print(b[:5])
+
+#Slice To the End
+print(b[2:])
+
+#Negative Indexing
+b = "Hello, World!"
+print(b[-5:-2])
+
+#upper case
+print(b.upper())
+
+#lower case
+print(b.lower())
+
+#Remove Whitespace
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+#Replace String
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+#Split String
+print(a.split(",")) # returns ['Hello', ' World!']
